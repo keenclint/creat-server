1699c67895fab997ea779d52e8fcbfa4ed987ab5
keenclint <142088502+keenclint@users.noreply.github.com>
2024-01-22 14:05:25 +0100

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,395 @@
+const express = require('express');
+const cors =  require('cors'); 
+const { MongoClient } = require("mongodb");
+const bodyParser = require('body-parser');
+const {
+  generateRandomString, credit, debit, 
+  getCredits, getDebits, patch_withdraw, 
+  getAllDashboard, getTransactions, getUser,
+  create_bene, create_other_bene,
+} = require('./random')
+require('dotenv').config()
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+async function getUsers(){
+
+  const uri = process.env.uri;  
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Users";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  
+  
+  try {
+    const documents = await collection.find().toArray();
+    if (documents === null) {
+      console.log(`Couldn't find any package.\n`);
+    } else {
+      return(JSON.stringify(documents))
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  await client.close(); 
+} 
+
+
+async function patch(user,amount){
+  const uri = process.env.uri;  
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Dashboard";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  const query = {username: user}
+  try {
+    const findOneResult = await collection.updateOne(query,{$set:{"balance":amount, "deposits":amount}});
+    if (findOneResult.modifiedCount === 1) {
+      console.log(`${user} updated with new price ${amount} .\n`);
+      return true
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  await client.close(); 
+
+}
+
+async function getDashBoard(_username){
+    const uri = process.env.uri;
+    
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Dashboard";
+
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+
+  const findOneQuery = { username: _username };
+
+  try {
+    const findOneResult = await collection.findOne(findOneQuery);
+    if (findOneResult === null) {
+      console.log(
+        `Couldn't find any package that contain ${_username} as an name.\n`
+      );
+    } else {
+      console.log(`Found a recipe with 'potato' as an ingredient:\n${JSON.stringify(findOneResult) }\n`)
+      return(JSON.stringify(findOneResult))
+      ;
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  // Make sure to call close() on your client to perform cleanup operations
+  await client.close(); 
+}   
+
+async function login(username,password) {
+  const uri = process.env.uri;
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Users";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  const findOneQuery = { username: username };
+
+  try {
+    const findOneResult = await collection.findOne(findOneQuery);
+    if (findOneResult !== null) {
+      if (findOneResult.password === password) {
+        await client.close();
+        return true;
+
+      } else {
+        await client.close();
+        return false;
+      }
+    } else {
+      await client.close();
+      return false;
+      
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one user: ${err}\n`);
+  }
+  // Make sure to call close() on your client to perform cleanup operations
+  await client.close();
+}
+
+
+async function register(_username, _password, _country, _email, _address, _mobile, _first, _last, _maiden) {
+  const uri = process.env.uri;
+  const client = new MongoClient(uri);  
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Users";
+  const database = client.db(dbName);
+  const user_collection = database.collection(collectionName);
+  const dashboard_collection = database.collection("Dashboard");
+
+  const acc_number = generateRandomString();
+  const user = {
+    username: _username,
+    password: _password,
+    country: _country,
+    email: _email,
+    address: _address,
+    mobile: _mobile,
+    first_name: _first,
+    last_name: _last,
+    maiden_name: _maiden,
+    acc_num : acc_number
+  };
+
+  const dashboard = {
+    account: acc_number, // should be 15
+    balance : 0,
+    deposits: 0,
+    transactions: 0,
+    fdr: 0,
+    dps: 0,
+    loan: 0,
+    username: _username,
+    withdrawals:0
+  }
+  try {
+    const insertOneUser = await user_collection.insertOne(user);
+    const insertManyDashboard = await dashboard_collection.insertOne(dashboard);
+    //console.log(`${user.username} successfully inserted.\n`);
+    console.log(`${dashboard.account} successfully inserted.\n`);
+    await client.close();
+    return true;
+  } catch (err) {
+    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+  }
+  await client.close();
+
+}
+
+
+async function root(username,password) {
+  const uri = process.env.uri;
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "Users";
+  const collectionName = "Admins";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  const findOneQuery = { username: username };
+
+  try {
+    const findOneResult = await collection.findOne(findOneQuery);
+    if (findOneResult !== null) {
+      if (findOneResult.password === password) {
+        await client.close();
+        return true;
+
+      } else {
+        await client.close();
+        return false;
+      }
+    } else {
+      await client.close();
+      return false;
+      
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one user: ${err}\n`);
+  }
+  // Make sure to call close() on your client to perform cleanup operations
+  await client.close();
+}
+
+
+app.get('/dashboard/:user', (req,res)=>{
+    async function getMyDashboard(){
+        const { user } = req.params;
+        const data = await getDashBoard(user);
+        res.send({data:data})
+    }getMyDashboard()
+})
+
+
+app.get('/transactions/:user', (req,res)=>{
+  async function getMyTransactions(){
+      const { user } = req.params;
+      const data = await getTransactions(user);
+      res.send({data:data})
+  }getMyTransactions()
+})
+
+
+app.post('/register',(req,res)=>{
+  async function create_account() {
+    console.log(req.body)
+    const { username, password, country, email, address, mobile,first_name,last_name,maiden_name } = req.body;
+    const success = await register(username,password,country,email,address,mobile,first_name,last_name,maiden_name);
+    if (success) {
+        res.send(success)
+    }else{
+      res.status(400).send("unable to save to database");
+      }
+  } create_account()
+})
+
+
+app.post('/credit',(req,res)=>{
+  async function create_credit() {
+    console.log(req.body)
+    const { username, amount, date } = req.body;
+    const response = await patch(user,amount)
+    const success = await credit(username,amount,date);
+    if (success) {
+        res.send(success)
+    }else{
+      res.status(400).send("unable to save to database");
+      }
+  } create_credit()
+})
+
+
+app.post('/debit',(req,res)=>{
+  async function create_debit() {
+    console.log(req.body)
+    const { username, amount, date } = req.body;
+    const response = await patch_withdraw(username,amount)
+    const success = await debit(username,amount,date);
+    if (success) {
+        res.send(success)
+    }else{
+        res.status(400).send("unable to save to database");
+    }
+  } create_debit()
+})
+
+
+app.post("/login", (req, res) => {
+  async function approve() {
+    console.log(req.body)
+    const { username, password } = req.body;
+    const response = await login(username, password)
+    if(response){
+      res.send(response)
+    }else{
+    res.status(400).send("wrong username or password");
+    }
+  }approve()
+})
+
+
+app.post("/root", (req, res) => {
+  async function approve() {
+    console.log(req.body)
+    const { username, password } = req.body;
+    const response = await root(username, password)
+    if(response){
+      res.send(response)
+    }else{
+    res.status(400).send("wrong username or password");
+    }
+  }approve()
+})
+
+
+
+app.get('/credits/:user', (req,res)=>{
+  async function getMyCredit(){
+      const { user } = req.params;
+      const data = await getCredits(user);
+      res.send({data:data})
+  }getMyCredit()
+})
+
+
+app.get('/debits/:user', (req,res)=>{
+  async function getMyUsers(){
+      const { user } = req.params;
+      const data = await getDebits(user);
+      res.send({data:data})
+  }getMyUsers()
+})
+
+
+
+app.get('/users', (req,res)=>{
+  async function getMyUsers(){
+      const data = await getUsers();
+      res.send({data:data})
+  }getMyUsers()
+})
+
+
+app.get('/user/:user', (req,res)=>{
+  async function getMyUser(){
+      const { user } = req.params;
+      const data = await getUser(user);
+      res.send({data:data})
+  }getMyUser()
+})
+
+app.get('/accounts', (req,res)=>{
+  async function getMyUsers(){
+      const data = await getAllDashboard();
+      res.send({data:data})
+  }getMyUsers()
+})
+
+app.post("/update", (req, res) => {
+  async function approve() {
+    console.log(req.body)
+    const { user, amount,date } = req.body;
+    const success = await credit(username,amount,date);
+    const response = await patch(user,amount)
+    if(response){
+      res.status(200).send(response)
+    }else{
+    res.status(400).send(false);
+    }
+  }approve()
+})
+
+app.post("/create_beneficiary", (req, res) => {
+  async function approve() {
+    console.log(req.body)
+    const { username, account_name, account_number, short_name } = req.body;
+    const success = await create_bene(username,account_name,account_number,short_name);
+    if(success){
+      res.status(200).send(success)
+    }else{
+    res.status(400).send(false);
+    }
+  }approve()
+})
+
+app.post("/other_beneficiary", (req, res) => {
+  async function approve() {
+    console.log(req.body)
+    const { username,bank_name,sort_code,routing_number, account_name, account_number, short_name } = req.body;
+    const success = await create_other_bene(username, bank_name, sortcode, routing_number, account_name,account_number,short_name);
+    if(success){
+      res.status(200).send(success)
+    }else{
+    res.status(400).send(false);
+    }
+  }approve()
+})
+
+
+const port = 8000
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}!.`)
+})
---
+++ server/random.js
@@ -0,0 +1,318 @@
+const { MongoClient } = require("mongodb");
+require('dotenv').config()
+
+
+function generateRandomString() {
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomString = '';
+
+  // Generate 2 random letters
+  for (let i = 0; i < 2; i++) {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    randomString += letters.charAt(randomIndex);
+  }
+
+  // Generate 13 random numbers
+  for (let i = 0; i < 13; i++) {
+    randomString += Math.floor(Math.random() * 10);
+  }
+
+  return randomString.toUpperCase();
+}
+
+async function credit(_username, _amount,date) {
+  const uri = process.env.uri;
+  const client = new MongoClient(uri);  
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Credits";
+  const database = client.db(dbName);
+  const tx_collection = database.collection(collectionName);
+  const Transactions_collection = database.collection("Transactions")
+
+  const sn = generateRandomString()
+  const tx = generateRandomString()
+  const txs = {
+    username: _username,
+    amount: _amount,
+    sn: sn,
+    tx: tx,
+  };
+  const transactions = {
+    username: _username,
+    amount: _amount,
+    sn: sn,
+    tx: tx,
+    type: "credit",
+    date: date, 
+    category: "Intra Bank",
+    receiptient: generateRandomString()
+  }
+  try {
+    const insertOneUser = await tx_collection.insertOne(txs);
+    const insertTransactions = await Transactions_collection.insertOne(transactions);
+    await client.close();
+    return true;
+  } catch (err) {
+    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+  }
+  await client.close();
+
+}
+
+async function debit(_username, _amount, date) {
+  const uri = process.env.uri;
+  const client = new MongoClient(uri);  
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Debits";
+  const database = client.db(dbName);
+  const tx_collection = database.collection(collectionName);
+  const Transactions_collection = database.collection("Transactions")
+
+  const sn = generateRandomString()
+  const tx = generateRandomString()
+  const txs = {
+    username: _username,
+    amount: _amount,
+    sn: sn,
+    tx: tx,
+  };
+  const transactions = {
+    username: _username,
+    amount: _amount,
+    sn: sn,
+    tx: tx,
+    date: date, 
+    type: "debit",
+    category: "Intra Bank",
+    receiptient: generateRandomString()
+  }
+  try {
+    const insertOneUser = await tx_collection.insertOne(txs);
+    const insertTransactions = await Transactions_collection.insertOne(transactions);
+    await client.close();
+    return true;
+  } catch (err) {
+    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+  }
+  await client.close();
+
+}
+
+
+async function getCredits(user){
+
+  const uri = process.env.uri;  
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Credits";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  
+  
+  try {
+    const documents = await collection.find({username:user}).toArray()
+    if (documents === null) {
+      console.log(`Couldn't find any package.\n`);
+    } else {
+      return(JSON.stringify(documents))
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  await client.close(); 
+} 
+
+
+async function getDebits(user){
+
+  const uri = process.env.uri;  
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Debits";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  
+  
+  try {
+    const documents = await collection.find({username:user}).toArray()
+    if (documents === null) {
+      console.log(`Couldn't find any package.\n`);
+    } else {
+      return(JSON.stringify(documents))
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  await client.close(); 
+} 
+
+
+async function patch_withdraw(user,amount){
+  const uri = process.env.uri;  
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Dashboard";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  const query = {username: user}
+
+  try {
+    const findOneResult = await collection.updateOne(query,{$set:{"withdrawals":amount}});
+    if (findOneResult.modifiedCount === 1) {
+      console.log(`${user} updated with new price ${amount} .\n`);
+      return true
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  await client.close(); 
+
+}
+
+async function getAllDashboard(){
+
+  const uri = process.env.uri;  
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Dashboard";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  
+  
+  try {
+    const documents = await collection.find().toArray();
+    if (documents === null) {
+      console.log(`Couldn't find any package.\n`);
+    } else {
+      return(JSON.stringify(documents))
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  await client.close(); 
+} 
+
+
+async function getTransactions(user){
+  const uri = process.env.uri;  
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Transactions";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  const query = {username: user}
+  try {
+    const documents = await collection.find(query).toArray()
+    if (documents === null) {
+      console.log(`Couldn't find any package.\n`);
+    } else {
+      return(JSON.stringify(documents))
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  await client.close();
+}
+
+
+async function getUser(user){
+
+  const uri = process.env.uri;  
+  const client = new MongoClient(uri);
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Users";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  const query = {username: user}  
+  try {
+    const documents = await collection.findOne(query);
+    if (documents === null) {
+      console.log(`Couldn't find any package.\n`);
+    } else {
+      return(JSON.stringify(documents))
+    }
+  } catch (err) {
+    console.error(`Something went wrong trying to find one document: ${err}\n`);
+  }
+  await client.close(); 
+} 
+
+async function create_bene(username,account_name,account_number,short_name ) {
+  const uri = process.env.uri;
+  const client = new MongoClient(uri);  
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "Beneficiaries";
+  const database = client.db(dbName);
+  const user_collection = database.collection(collectionName);
+  const user = {
+    username: username,
+    short_name: short_name,
+    account_name: account_name,
+    account_number : account_number
+  };
+
+  try {
+    const insertOneUser = await user_collection.insertOne(user);
+    await client.close();
+    return true;
+  } catch (err) {
+    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+  }
+  await client.close();
+
+}
+
+
+async function create_other_bene(username,bank_name,sort_code,routing_number,account_name,account_number,short_name ) {
+  const uri = process.env.uri;
+  const client = new MongoClient(uri);  
+  await client.connect();
+  const dbName = "CrestBank";
+  const collectionName = "OutsideBeneficiaries";
+  const database = client.db(dbName);
+  const user_collection = database.collection(collectionName);
+  const user = {
+    username: username,
+    bank_name:bank_name,
+    sort_code:sort_code,
+    routing_number:routing_number,
+    short_name: short_name,
+    account_name: account_name,
+    account_number : account_number
+  };
+
+  try {
+    const insertOneUser = await user_collection.insertOne(user);
+    await client.close();
+    return true;
+  } catch (err) {
+    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+  }
+  await client.close();
+
+}
+
+
+
+module.exports = {
+  generateRandomString,
+  credit,
+  debit,
+  getCredits,
+  getDebits,
+  patch_withdraw,
+  getAllDashboard,
+  getTransactions,
+  getUser,
+  create_bene,
+  create_other_bene,
+};
